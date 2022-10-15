2b2b20fb2a312aa8a34f38e5c01dc1e7e46fd07e
adilkhaleque <adilkhaleque429@gmail.com>
2022-10-15 00:09:58 -0400

    some of DataReader

--- src/data_structures/DataReader.java
+++ src/data_structures/DataReader.java
@@ -1,5 +1,11 @@
 package data_structures;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class DataReader {
 
     /** INSTRUCTIONS
@@ -19,8 +25,45 @@ public class DataReader {
      **/
 
     public static void main(String[] args) {
-        String textFilePath = System.getProperty("user.dir") + "/src/data_structures/data/self-driving-car.txt";
+        //String textFilePath = System.getProperty("user.dir") + "/src/data_structures/data/self-driving-car.txt";
+        String textFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator +
+                "data_structures" + File.separator + "data" + File.separator + "self-driving-car.txt";
 
+        File file = new File(textFilePath);
+
+        try (FileInputStream fis = new FileInputStream(file);
+             InputStreamReader isr = new InputStreamReader(fis);
+             BufferedReader br = new BufferedReader(isr)){
+
+            String line;
+
+            LinkedList<String> myLL = new LinkedList<>();
+            Stack<String> myStack = new Stack<>();
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                String[] words = line.split(" ");
+                myLL.addAll(Arrays.asList(words));
+                myStack.addAll(Arrays.asList(words));
+            }
+
+            Iterator<String> myIterator1 = myLL.iterator();
+            while (myIterator1.hasNext()) {
+                System.out.println(myIterator1.next());
+            }
+
+            Iterator<String> myIterator2 = myStack.iterator();
+            while (myIterator2.hasNext()) {
+                System.out.println(myIterator2.next());
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("NO FILE EXISTS AT THE PROVIDED PATH!");
+        } catch (IOException e) {
+            System.out.println("COULD NOT READ FROM FILE. PLEASE CHECK YOUR FILE IS NOT CORRUPTED");
+        }
     }
+
+
 
 }
