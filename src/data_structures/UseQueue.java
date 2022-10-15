efb16f1838ec71986ba6518a4117cf76580b9240
adilkhaleque <adilkhaleque429@gmail.com>
2022-10-15 03:46:34 -0400

    UseQueue

--- src/data_structures/UseQueue.java
+++ src/data_structures/UseQueue.java
@@ -1,5 +1,12 @@
 package data_structures;
 
+import databases.SharedStepsDatabase;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+
 public class UseQueue {
 
     /** INSTRUCTIONS
@@ -11,8 +18,49 @@ public class UseQueue {
      */
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
+        Queue<Object> numbers = new ArrayDeque<>();
+
+        numbers.add(0);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        System.out.println(numbers);
+        System.out.println();
+
+        for (Object n : numbers) {
+            System.out.println(n);
+        }
+
+        numbers.remove(5);
+        System.out.println(numbers);
+
+        System.out.println(numbers.peek());
+
+        System.out.println(numbers.poll());
+        System.out.println(numbers);
+        System.out.println(numbers.poll());
+        System.out.println(numbers);
+        System.out.println();
+
+        Iterator<Object> myIterator1 = numbers.iterator();
+
+        while (myIterator1.hasNext()) {
+            System.out.println(myIterator1.next());
+        }
+
+        SharedStepsDatabase ssdb = new SharedStepsDatabase();
+        ssdb.insertQueue("Queue_Example", "queue_numbers", numbers);
+        String queueQuery = "SELECT * FROM Queue_Example";
+
+        List<String> results = ssdb.executeQueryReadAllSingleColumn(queueQuery, "queue_numbers");
+
+        for (String queueResults: results) {
+            System.out.println(queueResults);
+        }
     }
 
 }
